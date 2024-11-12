25a786ef621a96377d7b962c09c4a7ce86ba8fd0
luchinsol <luisusca.sol@gmail.com>
2024-11-11 23:37:56 -0500

    post

--- api_cliente/models/cliente_model.js
+++ api_cliente/models/cliente_model.js
@@ -18,6 +18,18 @@ const modelCliente ={
         } catch (error) {
             throw new Error(`Error query get: ${error}`)
         }
+    },
+    postCliente : async () => {
+        try {
+            const resultado = await db_pool.one(`
+                INSERT INTO public.cliente (usuario_id,nombre,apellidos,direccion,telefono,email,distrito,ruc,fecha_nacimiento,
+                fecha_creacion_cuenta,sexo,dni,codigo,calificacion,saldo_beneficios,direccion_empresa,suscripcion,nombre_empresa,frecuencia,quiereretirar,medio_retiro,banco_retiro,numero_cuenta)
+                 VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23) RETURNING *`)
+            return resultado
+
+        } catch (error) {
+            throw new Error(`Error query post ${error}`)
+        }
     } 
 }
 
