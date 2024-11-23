8cf9341f6d27091bdc8c408ae09fcdef89282f13
luchinsol <luisusca.sol@gmail.com>
2024-11-23 00:05:18 -0500

    TODO

--- api_gateway/index.js
+++ api_gateway/index.js
@@ -74,7 +74,8 @@ import { createClient } from 'redis';
 
 // Configuración de Redis
 const redisClient = createClient({
-    url: 'redis://127.0.0.1:6379',
+    url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`,
+    //url: 'redis://127.0.0.1:6379',
     socket: {
         reconnectStrategy: (retries) => {
             const delay = Math.min(retries * 100, 3000); // Máximo 3 segundos entre reconexiones
