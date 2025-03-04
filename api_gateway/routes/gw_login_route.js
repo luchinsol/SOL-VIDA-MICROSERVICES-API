7cb7c635dfee7b3db4fcbfc1feea073c69d20318
luchinsol <luisusca.sol@gmail.com>
2025-03-04 14:22:00 -0500

    post user conductor

--- api_gateway/routes/gw_login_route.js
+++ api_gateway/routes/gw_login_route.js
@@ -1,9 +1,10 @@
-import { postLoginController, postUserExistController } from '../controllers/gw_login_controller.js'
+import { postLoginController, postNewUserConductorControllerGW, postUserExistController } from '../controllers/gw_login_controller.js'
 import express from 'express'
 
 const routerGWLogin = express.Router()
 
 routerGWLogin.post('/apigw/v1/login',postLoginController)
 routerGWLogin.post('/apigw/v1/user',postUserExistController)
+routerGWLogin.post('/apigw/v1/register_conductor',postNewUserConductorControllerGW)
 
 export default routerGWLogin
--- microservice_auth/routes/login_route.js
+++ microservice_auth/routes/login_route.js
@@ -1,11 +1,12 @@
 
 import express from 'express'
-import { getTelefonos,existUserController, loginController } from '../controllers/login_controller.js'
+import { getTelefonos,existUserController, loginController, postUserController } from '../controllers/login_controller.js'
 
 const routerLogin = express.Router()
 
 routerLogin.post('/login',loginController)
 routerLogin.post('/user',existUserController)
 routerLogin.get('/user_telefono/:id',getTelefonos)
+routerLogin.post('/user_new',postUserController)
 
 export default routerLogin
